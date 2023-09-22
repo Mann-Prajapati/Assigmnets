e964db5ee9d899333b9e6507f44299a987a8cd23
Mann Prajapati <129380158+Mann-Prajapati@users.noreply.github.com>
2023-09-22 16:10:27 +0530

    Assignment_Q-11.c

--- Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-11.c
+++ Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-11.c
@@ -1,6 +1,28 @@
 /*11). WAP to show difference between Structure and Union. */
 #include<stdio.h>
+#include<string.h>
+struct employee {
+	int id;
+	char name[100];
+};
+union emp {
+	int e_id;
+	char e_name[100];
+};
 int main()
 {
+	struct employee s1;
+	union emp u1;
+	//struct
+	printf("Enter Id  For First Employee And Name : ");
+	scanf("%d %s",&s1.id,&s1.name);
+	printf("Employee Id Is %d And Name Is %s",s1.id,s1.name);
+	//union
+	printf("\nEnter The Id For First Employee : ");
+	scanf("%d",&u1.e_id);
+	printf("First Employee Id Is %d",u1.e_id);
+	printf("\nEnter The Name Of First Employee : ");
+	scanf("%s",&u1.e_name);
+	printf("First Employee Name Is %s",u1.e_name);
 	return 0;
 }
