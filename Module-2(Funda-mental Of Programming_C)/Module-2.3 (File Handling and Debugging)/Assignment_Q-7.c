aaecee60bcaa21e167cfc5c8701b17ae5b22da2c
Mann Prajapati <129380158+Mann-Prajapati@users.noreply.github.com>
2023-10-02 15:35:54 +0530

    Assignment_Q-7.c

--- Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-7.c
+++ Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-7.c
@@ -2,5 +2,14 @@
 #include<stdio.h>
 int main()
 {
+	char str[100];
+	int i,length;
+	printf("Enter The String : ");
+	scanf("%s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		length++;
+	}
+	printf("\n Length of Input String : %d",i);
 	return 0;
 }
