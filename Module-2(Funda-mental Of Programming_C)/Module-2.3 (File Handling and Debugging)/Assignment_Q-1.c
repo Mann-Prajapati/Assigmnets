270238d24094107a708af8a65167c4b037d3c87f
Mann Prajapati <129380158+Mann-Prajapati@users.noreply.github.com>
2023-10-02 15:16:38 +0530

    Assignment_Q-1.c

--- Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-1.c
+++ Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-1.c
@@ -2,5 +2,24 @@
 #include<stdio.h>
 int main()
 {
+	int arr[10],n,i;
+	printf("Enter the number of elements : ");
+	scanf("%d", &n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("Enter number%d: ", i + 1);
+    	scanf("%d", &arr[i]);
+	}
+	
+	for (i = 1; i < n; ++i)
+	{
+		if(arr[0]<arr[i])
+		{
+			arr[0]=arr[i];
+		}
+    }
+	
+	printf("Largest element = %d", arr[0]);
 	return 0;
 }
