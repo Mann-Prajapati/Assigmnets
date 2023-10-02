0b8a9a735c332fedbe6fea9290af1e599c97e27a
Mann Prajapati <129380158+Mann-Prajapati@users.noreply.github.com>
2023-10-02 15:18:16 +0530

    Assignment_Q-4.c

--- Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-4.c
+++ Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-4.c
@@ -2,5 +2,20 @@
 #include<stdio.h>
 int main()
 {
+	int n;
+	printf("Enter a positive integer : ");
+	scanf("%d",&n);
+	printf("Factorial Of %d = %d",n,add(n));
 	return 0;
+}
+int add(int n)
+{
+	if(n>=1)
+	{
+		return n*add(n-1);
+	}
+	else
+	{
+		return 1;
+	}
 }
