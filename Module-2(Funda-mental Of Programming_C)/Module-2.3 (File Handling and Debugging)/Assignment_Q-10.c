496a6abaa9e70c76869304893c084ee1236dff88
Mann Prajapati <129380158+Mann-Prajapati@users.noreply.github.com>
2023-10-03 16:04:18 +0530

    Assignment_Q-10.c

--- Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-10.c
+++ Module-2(Funda-mental Of Programming_C)/Module-2.3 (File Handling and Debugging)/Assignment_Q-10.c
@@ -1,7 +1,38 @@
 /*10). Write a program of structure for five employee that provides the following
     information -print and display empno, empname, address and age */
 #include<stdio.h>
+struct employee{
+	
+	int e_num;
+	char e_name[10];
+	char e_address[20];
+	int e_age;
+};
 int main()
 {
+	struct employee emp[5];
+	int i,n=5;
+	printf("----------Print The Employee Details----------");
+	for(i=0;i<n;i++)
+	{
+		printf("\nEnter The Employee Number : ");
+		scanf("%d",&emp[i].e_num);
+		printf("Enter The Employee Name : ");
+		scanf("%s",&emp[i].e_name);
+		printf("Enter The Employee Age : ");
+		scanf("%d",&emp[i].e_age);
+		printf("Enter The Employee Address : ");
+		scanf("%s",&emp[i].e_address);
+	}
+	printf("----------Display the Employee Details----------");
+	for(i=0;i<n;i++)
+	{
+		printf("\nEmployee Number Is %d",emp[i].e_num);
+		printf("\nEmployee Name Is %s",emp[i].e_name);
+		printf("\nEmployee Age Is %d",emp[i].e_age);
+		printf("\nEmployee Address Is %s",emp[i].e_address);
+		printf("\n");
+	}
+	
 	return 0;
 }
